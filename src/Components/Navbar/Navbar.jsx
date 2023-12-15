187a2f308af61de85719712e5853f72f7e3ec76f
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-12-15 19:38:11 +0100

    Tweak on detail

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+
+import { FaTimes } from "react-icons/fa";
+import { CgMenuRight } from "react-icons/cg";
+import { IconContext } from "react-icons";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  NavIcon,
+  MobileIcon,
+  NavMenu,
+  NavLinks,
+  NavItem,
+} from "./NavbarStyle";
+import { useLocation, useHistory } from "react-router-dom";
+import { data } from "../../data/NavbarData";
+
+function Navbar() {
+  const [show, setShow] = useState(false);
+
+  let history = useHistory();
+  let location = useLocation();
+
+  function handleClick() {
+    setShow((show) => !show);
+  }
+
+  function scrollTo(id) {
+    const element = document.getElementById(id);
+
+    element.scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+
+  function closeMobileMenu(to, id) {
+    if (id && location.pathname === "/") scrollTo(id);
+
+    history.push(to);
+    setShow(false);
+  }
+
+  return (
+    <IconContext.Provider value={{ color: "#fff" }}>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">
+            <NavIcon src="../../../public/assets/logo.png" />
+            Delta
+          </NavLogo>
+
+          <MobileIcon onClick={handleClick}>
+            {show ? <FaTimes /> : <CgMenuRight />}
+          </MobileIcon>
+
+          <NavMenu show={show}>
+            {data.map((el, index) => (
+              <NavList
+                el={el}
+                key={index}
+                onClick={() => closeMobileMenu(el.to, el.id)}
+              />
+            ))}
+          </NavMenu>
+        </NavbarContainer>
+      </Nav>
+    </IconContext.Provider>
+  );
+}
+
+function NavList({ el, onClick }) {
+  return (
+    <NavItem>
+      <NavLinks onClick={onClick}>{el.text}</NavLinks>
+    </NavItem>
+  );
+}
+
+export default Navbar;
