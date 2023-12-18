453f993c129369a37e6cd038007f822806d0151f
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-12-18 21:27:48 +0100

    Features data implementation

--- src/Components/Features/Features.jsx
+++ src/Components/Features/Features.jsx
@@ -1,9 +1,37 @@
+import { Container, Section } from "../../GlobalStyles";
+import {
+  FeatureText,
+  FeatureTitle,
+  FeatureWrapper,
+  FeatureColumn,
+  FeatureImageWrapper,
+  FeatureTextWrapper,
+  FeatureName,
+} from "./FeaturesStyle";
+
+import { featuresData } from "../../data/FeaturesData";
+
 function Features() {
   return (
-    <div>
-      Features
-    </div>
-  )
+    <Section>
+      <Container>
+        <FeatureTextWrapper>
+          {featuresData.map((feature, index) => (
+            <FeatureList feature={feature} key={index} />
+          ))}
+        </FeatureTextWrapper>
+      </Container>
+    </Section>
+  );
 }
 
-export default Features
+export default Features;
+
+
+function FeatureList() {
+  return <FeatureColumn>
+    <FeatureImageWrapper>
+      Gift
+    </FeatureImageWrapper>
+  </FeatureColumn>
+}
