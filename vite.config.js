88048bb22db06a3592f24cf5c467a149933cc27a
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-12-19 21:32:52 +0100

    Configuration on vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import eslint from "vite-plugin-eslint"
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import eslint from "vite-plugin-eslint";
+// import { resolve } from "path";
+// import { fs } from "fs/promises";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   resolve: {
+//     alias: {
+//       src: resolve(__dirname, "src"),
+//     },
+//   },
+//   esbuild: {
+//     loader: "jsx",
+//     include: /src\/.*\.jsx?$/,
+//     exclude: [],
+//   },
+//   optimizeDeps: {
+//     esbuildOptions: {
+//       plugins: [
+//         {
+//           name: "load-js-files-as-jsx",
+//           setup(build) {
+//             build.onLoad({ filter: /src\\.*\.js$/ }, async (args) => ({
+//               loader: "jsx",
+//               contents: await fs.readFile(args.path, "utf8"),
+//             }));
+//           },
+//         },
+//       ],
+//     },
+//   },
+
+//   plugins: [react(), eslint()],
+// });
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
+// import { resolve } from 'path';
+// import { readFileSync } from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
-})
+});
