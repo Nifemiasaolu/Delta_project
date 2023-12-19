89117630738493bdc59697849aa25700d0f17674
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-12-19 21:33:18 +0100

    Features details in play

--- src/Components/Features/Features.jsx
+++ src/Components/Features/Features.jsx
@@ -12,26 +12,50 @@ import {
 import { featuresData } from "../../data/FeaturesData";
 
 function Features() {
+  const initial = {
+    y: 40,
+    opacity: 0,
+  };
+  const animate = {
+    y: 0,
+    opacity: 1,
+  };
+
   return (
-    <Section>
+    <Section smPadding="50px 10px" position="relative" inverse id="about">
       <Container>
         <FeatureTextWrapper>
-          {featuresData.map((feature, index) => (
-            <FeatureList feature={feature} key={index} />
-          ))}
+          <FeatureTitle>What We Offer</FeatureTitle>
         </FeatureTextWrapper>
+
+        <FeatureWrapper>
+          {featuresData.map((feature, index) => (
+            <FeatureList
+              initial={initial}
+              animate={animate}
+              transition={{ duration: 0.5 + index * 0.1 }}
+              feature={feature}
+              key={index}
+            />
+          ))}
+        </FeatureWrapper>
       </Container>
     </Section>
   );
 }
 
-export default Features;
 
-
-function FeatureList() {
-  return <FeatureColumn>
-    <FeatureImageWrapper>
-      Gift
-    </FeatureImageWrapper>
-  </FeatureColumn>
+function FeatureList({ initial, animate, transition, feature }) {
+  return (
+    <FeatureColumn initial={initial} animate={animate} transition={transition}>
+      <FeatureImageWrapper className={feature.imgClass}>
+        {feature.icon}
+      </FeatureImageWrapper>
+      <FeatureName>{feature.name}</FeatureName>
+      <FeatureText>{feature.description}</FeatureText>
+    </FeatureColumn>
+  );
 }
+// console.log(Response)
+
+export default Features;
