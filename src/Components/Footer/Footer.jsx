dc0d898d28204f813aa5eec30a918c39f82947ef
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-12-21 23:34:56 +0100

    Footer component detailed

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,61 @@
+import { Section, Row } from "../../GlobalStyles";
+import {
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+  FooterLogo,
+  SocialIcon,
+  FooterRights,
+  FooterSocialIcon,
+  FooterWrapper,
+  FooterAddress,
+  FooterColumn,
+  FooterGrid,
+} from "./FooterStyle";
+import { footerData, footerSocialData } from "../../data/FooterData";
+
 function Footer() {
   return (
-    <div>
-      Footer
-    </div>
-  )
+    <Section padding="4rem 0 2rem 0">
+      <FooterWrapper>
+        <FooterGrid justify="space-between">
+          <FooterColumn id="footerLogo">
+            <FooterLogo to="/">
+              <SocialIcon src="../../../public/assets/logo.png" />
+              Delta
+            </FooterLogo>
+
+            <FooterAddress>
+              69 street, Delta Building, US Road, Los Angeles 123.
+            </FooterAddress>
+
+            <Row align="center" margin="auto 0 0 0" gap="1rem">
+              {footerSocialData.map((social, index) => (
+                <FooterSocialIcon
+                  key={index}
+                  href="/"
+                  target="_blank"
+                  aria-label={social.name}
+                >
+                  {social.icon}
+                </FooterSocialIcon>
+              ))}
+            </Row>
+          </FooterColumn>
+
+          {footerData.map((footerItem, index) => (
+            <FooterLinkItems key={index}>
+              <FooterLinkTitle>{footerItem.title}</FooterLinkTitle>
+              {footerItem.links.map((link, linkIndex) => (
+                <FooterLink key={linkIndex}>{link}</FooterLink>
+              ))}
+            </FooterLinkItems>
+          ))}
+        </FooterGrid>
+        <FooterRights>Delta &copy; 2021</FooterRights>
+      </FooterWrapper>
+    </Section>
+  );
 }
 
-export default Footer
+export default Footer;
