d1c79f03b68e39252fb97bf1dbe8b333ceee817f
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-12-21 21:08:12 +0100

    Carousel content in display

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,9 +1,17 @@
+import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
+import { IconContext } from "react-icons";
+import { data, sliderSettings } from "../../data/CarouselData";
+import { Row, Heading, Section, TextWrapper } from "../../GlobalStyles";
+import {
+  CarouselImage,
+  ImageWrapper,
+  ButtonContainer,
+  ReviewSlider,
+  CardButton,
+} from "./CarouselStyle";
+
 function Carousel() {
-  return (
-    <div>
-      Carousel
-    </div>
-  )
+  return <div>Carousel</div>;
 }
 
-export default Carousel
+export default Carousel;
