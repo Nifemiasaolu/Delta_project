5016dec2591c0b9448f7c0b27b6fce0093a6343e
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-12-23 20:02:50 +0100

    Pricing action

--- src/Components/Pricing/Pricing.jsx
+++ src/Components/Pricing/Pricing.jsx
@@ -1,9 +1,63 @@
+import { Button, Heading, TextWrapper } from "../../GlobalStyles";
+import { IconContext } from "react-icons";
+import {
+  PricingSection,
+  PricingWrapper,
+  PricingHeading,
+  PricingContainer,
+  PricingCardInfo,
+  PricingCardPlan,
+  PricingCardCost,
+  PricingCardFeatures,
+  PricingCardText,
+  PricingCardFeature,
+  PricingCard,
+} from "./PricingStyle";
+import { pricingData } from "../../data/PricingData";
+
 function Pricing() {
   return (
-    <div>
-      Pricing
-    </div>
-  )
+    <IconContext.Provider value={{ color: "#a9b3c1", size: "1rem" }}>
+      <PricingSection>
+        <PricingWrapper>
+          <PricingHeading mb="1rem">Pick Your Best Option</PricingHeading>
+
+          <TextWrapper
+            mb="2rem"
+            weight="600"
+            size="1rem"
+            color="white"
+            align="center"
+          >
+            Create, maintain and store your data with Delta
+          </TextWrapper>
+
+          <PricingContainer>
+            {pricingData.map((card, index) => (
+              <PricingList key={index} card={card} />
+            ))}
+          </PricingContainer>
+        </PricingWrapper>
+      </PricingSection>
+    </IconContext.Provider>
+  );
 }
 
-export default Pricing
+export default Pricing;
+
+function PricingList({ card }) {
+  return (
+    <PricingCard>
+      <PricingCardInfo>
+        <PricingCardPlan>{card.title}</PricingCardPlan>
+        <PricingCardCost>{card.price}</PricingCardCost>
+        <PricingCardText>{card.description}</PricingCardText>
+        <PricingCardFeatures>
+          {card.features.map((feature, index) => (
+            <PricingCardFeature key={index}>{feature}</PricingCardFeature>
+          ))}
+        </PricingCardFeatures>
+      </PricingCardInfo>
+    </PricingCard>
+  );
+}
