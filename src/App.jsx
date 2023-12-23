00a0a74e43601f2ea7c855604c4b84e9fced1b62
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-12-23 20:01:14 +0100

    Updated component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import GlobalStyle from "./GlobalStyles";
-import { Home, Pricing, Signup } from "./Pages";
+import { Home, PricingPage, Signup } from "./Pages";
 import { Footer, Navbar } from "./Components";
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/pricing" element={<PricingPage />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
         <Footer/>
